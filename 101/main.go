ec79132102d4d71b1cbaa89643fad06002a9aa33
林涛 <mrqter@gmail.com>
2021-02-23 17:30:12 +0800

    101

---
+++ 101/main.go
@@ -0,0 +1,54 @@
+//给定一个二叉树，检查它是否是镜像对称的。
+//
+//
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+//
+//
+//
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+//
+//
+//
+//
+// 进阶：
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return isMirror(root, root)
+}
+
+func isMirror(t1, t2 *TreeNode) bool {
+	if t1 == nil && t2 == nil {
+		return true
+	}
+
+	if t1 == nil || t2 == nil {
+		return false
+	}
+
+	return (t1.Val == t2.Val) && isMirror(t1.Left, t2.Right) && isMirror(t1.Right, t2.Left)
+}
